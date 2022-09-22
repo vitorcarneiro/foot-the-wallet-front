055650fd0f5013db370cbbdd07fa32c9ccd2e698
Vitor Carneiro Silva de Oliveira <vitorcarneiro.sdo@gmail.com>
2022-09-22 12:22:38 +0000

    feat: layout login box

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -2,53 +2,64 @@ import React from 'react';
 import { BsApple, BsFacebook, BsGithub } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
 import {
-  LoginSignupBoxContainer,
+  Container,
+  LoginBoxContainer,
   Border,
   BoxBackground,
   UserBox,
   SocialMediaContainer,
+  SignupButton,
 } from './style';
 
 function LoginBox() {
   return (
-    <LoginSignupBoxContainer>
-      <Border />
-      <BoxBackground>
-        <h2>Bem vindo de volta!</h2>
-        <form>
-          <UserBox>
-            <input type="text" name="" required />
-            <label htmlFor="temp-id">Email</label>
-          </UserBox>
-          <UserBox>
-            <input type="password" name="" required />
-            <label htmlFor="temp-id">Senha</label>
-          </UserBox>
-          <a href="/home">
-            <span />
-            <span />
-            <span />
-            <span />
-            Submit
-          </a>
-        </form>
-        <h3>- - - - - - OU - - - - - -</h3>
-        <SocialMediaContainer>
-          <button type="submit">
-            <FcGoogle />
-          </button>
-          <button type="submit">
-            <BsFacebook color="#4267B2" />
-          </button>
-          <button type="submit">
-            <BsApple color="#333" />
-          </button>
-          <button type="submit">
-            <BsGithub color="#6e5494" />
-          </button>
-        </SocialMediaContainer>
-      </BoxBackground>
-    </LoginSignupBoxContainer>
+    <Container>
+      <LoginBoxContainer>
+        <Border />
+        <BoxBackground>
+          <h2>Bem vindo de volta!</h2>
+          <form>
+            <UserBox>
+              <input type="text" name="" required />
+              <label htmlFor="temp-id">Email</label>
+            </UserBox>
+            <UserBox>
+              <input type="password" name="" required />
+              <label htmlFor="temp-id">Senha</label>
+            </UserBox>
+            <a href="/home">
+              <span />
+              <span />
+              <span />
+              <span />
+              Submit
+            </a>
+          </form>
+          <h3>- - - - - - OU - - - - - -</h3>
+          <SocialMediaContainer>
+            <button type="submit">
+              <FcGoogle />
+            </button>
+            <button type="submit">
+              <BsFacebook color="#4267B2" />
+            </button>
+            <button type="submit">
+              <BsApple color="#333" />
+            </button>
+            <button type="submit">
+              <BsGithub color="#6e5494" />
+            </button>
+          </SocialMediaContainer>
+        </BoxBackground>
+      </LoginBoxContainer>
+
+      <LoginBoxContainer>
+        <Border />
+        <BoxBackground>
+          <SignupButton>Registrar</SignupButton>
+        </BoxBackground>
+      </LoginBoxContainer>
+    </Container>
   );
 }
 
--- src/components/LoginBox/style.tsx
+++ src/components/LoginBox/style.tsx
@@ -1,12 +1,28 @@
 import styled from 'styled-components';
 
-export const LoginSignupBoxContainer = styled.div`
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: space;
+  justify-content: space-evenly;
+`;
+
+export const LoginBoxContainer = styled.div`
   width: 400px;
   height: 450px;
 
   position: relative;
   align-self: flex-start;
   justify-self: right;
+
+  :last-child {
+    height: 120px;
+
+    * {
+      height: 120px;
+    }
+  }
 `;
 
 export const Border = styled.div`
@@ -69,7 +85,8 @@ export const BoxBackground = styled.div`
       text-transform: uppercase;
       overflow: hidden;
       transition: 0.5s;
-      margin-top: 40px;
+      margin-top: 15px;
+      margin-bottom: 20px;
       letter-spacing: 4px;
 
       :hover {
@@ -240,7 +257,8 @@ export const SocialMediaContainer = styled.div`
     height: 60px;
     border-radius: 5px;
     border: 1px solid #a5a4a4;
-    background-color: #ffffff;
+    background-color: ${({ theme }) => theme.colors.text};
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 5px 15px;
 
     display: flex;
     align-items: center;
@@ -252,4 +270,23 @@ export const SocialMediaContainer = styled.div`
       height: 35px;
     }
   }
+`;
+
+export const SignupButton = styled.div`
+  width: 100%;
+  height: 100% !important;
+
+  background-color: ${({ theme }) => theme.colors.text};
+  border-radius: 20px;
+  border: 1px solid #a5a4a4;
+
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 5px 15px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  text-transform: uppercase;
+
+  cursor: pointer;
 `;
